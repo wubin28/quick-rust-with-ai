4974d6941f6ebbb23f48eaa801bb629e052e9cd9
Bin Wu <wubin28@gmail.com>
2024-12-03 21:27:27 +0800

    feat: blinked the LED using cortex_m::asm::delay

--- ch02/b1l/src/main.rs
+++ ch02/b1l/src/main.rs
@@ -7,12 +7,20 @@ use embedded_hal::digital::OutputPin;
 use microbit::board::Board;
 use panic_halt as _;
 
+use cortex_m::asm::delay;
+
 #[entry]
 fn main() -> ! {
     let mut board = Board::take().unwrap();
 
-    board.display_pins.col4.set_low().unwrap();
-    board.display_pins.row4.set_high().unwrap();
+    loop {
+        board.display_pins.col4.set_low().unwrap();
+        board.display_pins.row4.set_high().unwrap();
 
-    loop {}
+        delay(1_000_000);
+
+        board.display_pins.row4.set_low().unwrap();
+
+        delay(1_000_000);
+    }
 }
